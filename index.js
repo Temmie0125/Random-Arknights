94d9822367c9ba1e8fe2d66e798e36fb6a7463e9
Temmie <1179755948@qq.com>
2025-12-25 19:17:25 +0800

    main

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: Temmie 1179755948@qq.com
+ * @Date: 2025-12-25 19:06:17
+ * @LastEditors: Temmie 1179755948@qq.com
+ * @LastEditTime: 2025-12-25 19:16:30
+ * @FilePath: \实验与作业e:\bot\plugins\rndarks\index.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 logger.info(`---------=.=---------`)
 logger.info(`转生干员插件载入中`)
 import fs from 'node:fs'
@@ -5,6 +13,7 @@ import fs from 'node:fs'
 import { randomArknights } from './apps/main.js'
 
 export { randomArknights }
+
 
 // Yunzai插件规范
 export default randomArknights
